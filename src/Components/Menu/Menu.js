859e7cd6b3564d87322903cd0a3ebbb6f249289f
Roberto Basulto <roberto.basulto085@gmail.com>
2021-05-25 18:26:17 -0400

    Changed the menu items into cards

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,8 +3,10 @@ import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { CardContent, Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,66 +22,97 @@ const useStyles = makeStyles((theme) => ({
   categoryItemsPadding: {
     paddingBottom: "15px",
   },
-  tags: {
-    marginLeft: 4,
-    marginRight: 4,
-  }
+  tagsGrid: {
+    align: "center",
+  },
 }));
 
+// Returns extras the menu item has
 function ItemExtras(props) {
   return props.extras.map((extra) => {
     return (
-      <Typography align="center" key={extra.info}>
-        {extra.info}.....${extra.price}
+      <Typography variant="body2" component="p" key={extra.info}>
+        {extra.info}
       </Typography>
     );
   });
 }
 
+// Returns a any tags the menu item has in a grid
 function ItemTags(props) {
-  const classes = useStyles();
-  return props.tags.map((tag) => {
-    return (
-      <Typography className={classes.tags} display="inline" key={tag}>
-        {tag}
-      </Typography>
-    );
-  });
+  return (
+    <Grid container>
+      {props.tags.map((tag) => {
+        return (
+          <Grid item xs={4}>
+            <Paper>
+              <Typography align="center">{tag}</Typography>
+            </Paper>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 
+// Returns the menu description
+function ItemDescription(props) {
+  return (
+    <Typography paragraph variant="body2" component="p">
+      {props.description}
+    </Typography>
+  );
+}
+
+// Returns the menu category item and its different components
 function CategoryItem(props) {
   const classes = useStyles();
 
+  // Checks if item has any extras
   const hasExtras = () => {
-    if (props.menuItem.extra.length > 0) {
-      return true;
-    } else {
+    if (props.menuItem.extra.length === 0) {
       return false;
+    } else {
+      return true;
     }
   };
 
+  // Checks if item has any tags
   const hasTags = () => {
-    if (props.menuItem.tags.length > 0) {
-      return true;
-    } else {
+    if (props.menuItem.tags.length === 0) {
       return false;
+    } else {
+      return true;
+    }
+  };
+
+  // Checks if item has a description
+  const hasDescription = () => {
+    if (props.menuItem.description === null) {
+      return false;
+    } else {
+      return true;
     }
   };
 
   return (
     <Grid item lg={4} md={6} xs={12} className={classes.categoryItemsPadding}>
-      <Typography align="center">{props.menuItem.name}</Typography>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {props.menuItem.name}
+          </Typography>
 
-      <Typography align="left">{props.menuItem.description}</Typography>
-
-      <Typography align="center">{props.menuItem.price}</Typography>
-
-      {hasExtras ? <ItemExtras extras={props.menuItem.extra} /> : <br />}
-      {hasTags ? <ItemTags tags={props.menuItem.tags} /> : <br />}
+          {hasDescription() ? <ItemDescription description={props.menuItem.description} /> : null}
+          {hasExtras() ? <ItemExtras extras={props.menuItem.extra} /> : null}
+          {hasTags() ? <ItemTags tags={props.menuItem.tags} /> : null}
+        </CardContent>
+      </Card>
     </Grid>
   );
 }
 
+// Returns a menu category and items in the category
 function MenuCategory(props) {
   const classes = useStyles();
   return (
@@ -119,7 +152,7 @@ export default function Menu() {
 
   return (
     <Container>
-      <Grid container>
+      <Grid container className={classes.root} spacing={4}>
         <Grid item xs={12}>
           <Typography variant="h3" align="center" gutterBottom>
             Menu
