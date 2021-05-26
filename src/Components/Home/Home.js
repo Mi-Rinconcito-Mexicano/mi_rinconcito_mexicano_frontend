479381b39c8aa6a1e3d82810918b03fcee64e9b4
Roberto Basulto <roberto.basulto085@gmail.com>
2021-05-25 21:10:21 -0400

    Created basic return for home and panaderia links to render

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+
+function Home() {
+    return (
+        <Typography>
+            Home Page
+        </Typography>
+    )
+}
+
+export default Home;
---
+++ src/Components/Panaderia/Panaderia.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+
+function Panaderia() {
+    return (
+        <Typography>
+            Panaderia
+        </Typography>
+    )
+}
+
+export default Panaderia;
